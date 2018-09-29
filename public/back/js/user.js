468a0ad3f5db6b2a324872c3ae5d633d68562ef1
xwakeup <619678987@qq.com>
2018-09-29 15:47:02 +0800

    用户模块-启用禁用功能完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,8 +1,11 @@
 $(function(){
   var currentPage=1;//当前页
   var pageSize=5;//每页多少条
+
+  var currentId;
+  var isDelete;
   // ajax请求后台数据
-  //一进入页面就请求一次数据
+  //1\一进入页面就请求一次数据
    render();
    function render(){
     $.ajax({
@@ -36,5 +39,40 @@ $(function(){
     })
    }
 
+
+   //2\点击启用禁用按钮,显示模态框,通过事件委托绑定事件
+   $('tbody').on('click','.btn',function(){
+     $('#userModal').modal("show");
+     //获取当前数据的id
+     currentId=$(this).parent().data("id");
+     // 点击禁用按钮,将用户改成禁用状态,isDelete变成0
+     isDelete=$(this).hasClass("btn-danger")?0:1;
+   })
+
+   //点击模态框的确定按钮,修改用户状态,发送ajax请求
+   $('.submitBtn').on("click",function(){
+     console.log(currentId,isDelete);
+     $.ajax({
+       type:"post",
+       url:"/user/updateUser",
+       data:{
+          id:currentId,
+          isDelete:isDelete
+       },
+       dataType:"json",
+       success:function(info){
+         console.log(info);
+         if(info.success){
+           //关闭模态框
+           $('#userModal').modal("hide");
+           //重新渲染
+           render();
+         }
+       }
+     })
+     
+   })
+
+});
+
  
-})
